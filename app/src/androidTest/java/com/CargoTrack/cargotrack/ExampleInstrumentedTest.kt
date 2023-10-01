8dc3eb8ec835fb292e4bd21e5d0087bdc14e3c9c
Saffiyah Mansoor <ST10083932@vcconnect.edu.za>
2023-10-02 00:04:11 +0200

    Login/Signup

--- app/src/androidTest/java/com/CargoTrack/cargotrack/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/CargoTrack/cargotrack/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.CargoTrack.cargotrack", appContext.packageName)
+        assertEquals("com.CargoTrack.com.com.cargotrack.cargotrack.com.cargotrack.cargotrack", appContext.packageName)
     }
 }
